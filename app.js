becc3b979231ef0182a1414bf08b5e70f1bbb34d
Egor Miloserdov <emiloserdov@captiv8.io>
2024-06-14 01:00:21 +0400

    ы

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.getElementById('sendMessage').addEventListener('click', () => {
     if (window.Telegram.WebApp) {
-        Telegram.WebApp.sendData("Hello from the mini app!");
+        window.Telegram.WebApp.sendData("Hello from the mini app!");
     } else {
         alert("This function is only available within the Telegram app.");
     }
